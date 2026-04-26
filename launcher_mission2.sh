3c1855a2fdd6800def70a58a67c1920b22ed2344
Shamil <mohammedshamil99128@gmail.com>
2026-04-26 19:08:50 +0530

    Added the AeroTHON banner

--- launcher_mission2.sh
+++ launcher_mission2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the exact spawn coordinates for the drone
-export PX4_GZ_MODEL_POSE="232,80,0.3,0,0,0"
+export PX4_GZ_MODEL_POSE="230.5,80,0.3,0,0,0"
 
 # Launch the simulation
 echo "Starting Gazebo and PX4 SITL on mission2..."
