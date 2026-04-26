e0f3a0b079102fc1c774209b4ec622bdbc416279
Shamil <mohammedshamil99128@gmail.com>
2026-04-26 10:33:26 +0530

    Made look more realistic and added a launch file

---
+++ launcher_mission2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the exact spawn coordinates for the drone
+export PX4_GZ_MODEL_POSE="232,80,0.3,0,0,0"
+
+# Launch the simulation
+echo "Starting Gazebo and PX4 SITL on mission2..."
+make px4_sitl gz_x500_mission2
